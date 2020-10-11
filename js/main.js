39558628717f32267272860e4819e4fafa882a2d
Groud Frank <groudfrank@gmail.com>
2020-10-11 11:37:35 -0400

    Got the animation on the navigation tab working as I want it to. There is more to do like animating the different options under the menu but things are comming along nicely. Before I get to that though I want to start working on resizing and scaling the menu for mobile next

--- js/main.js
+++ js/main.js
@@ -1,9 +1,53 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-var NavMenuBtnWrapper = document.getElementById('nav-menu-btn-wrapper');
+var NavMenuBtn = document.getElementById('nav-menu-btn');
+var NavMenuWrapper = document.getElementById('nav-menu-wrapper');
+var TopNavMenuBtnLine = document.getElementById('top-nav-menu-btn-line');
+var BottomNavMenuBtnLine = document.getElementById('bottom-nav-menu-btn-line');
+var NavMenuLabel = document.getElementById('nav-menu-label');
 
-NavMenuBtnWrapper.addEventListener('click', () =>{
-    alert('clicked');
+// var addPropertyValue = (el, property, value) =>{
+//     el.style.property = value;
+// }
+
+NavMenuBtn.addEventListener('click', () =>{
+    if(NavMenuWrapper.classList.contains('hidden')){
+        // Fades in the navigation bar
+        NavMenuWrapper.classList.remove('hidden');
+        NavMenuWrapper.classList.remove('fade-out-nav-bar');
+        NavMenuWrapper.classList.add('fade-in-nav-bar');
+
+        // Transitions the navigation menu lines to an X.
+        if(TopNavMenuBtnLine.classList.contains('off-state')){
+            TopNavMenuBtnLine.classList.remove('off-state');
+            TopNavMenuBtnLine.classList.add('top-nav-btn-icon-transition');
+        }
+        if(BottomNavMenuBtnLine.classList.contains('off-state')){
+            BottomNavMenuBtnLine.classList.remove('off-state');
+            BottomNavMenuBtnLine.classList.add('bottom-nav-btn-icon-transition');
+        }
+        // add transition styles for the navigation menu label
+        NavMenuLabel.style.opacity = '0';
+        NavMenuLabel.style.transform = 'translateX(-20px)';
+    } else{
+        // Fades out the navigation bar
+        NavMenuWrapper.classList.add('hidden');
+        NavMenuWrapper.classList.add('fade-out-nav-bar');
+        NavMenuWrapper.classList.remove('fade-in-nav-bar');
+
+        if(TopNavMenuBtnLine.classList.contains('off-state') == false){
+            TopNavMenuBtnLine.classList.add('off-state');
+            TopNavMenuBtnLine.classList.remove('top-nav-btn-icon-transition');
+        }
+
+        if(BottomNavMenuBtnLine.classList.contains('off-state') == false){
+            BottomNavMenuBtnLine.classList.add('off-state');
+            BottomNavMenuBtnLine.classList.remove('bottom-nav-btn-icon-transition');
+        }
+        // add transition styles for the navigation menu label
+        NavMenuLabel.style.opacity = '1';
+        NavMenuLabel.style.transform = 'translateX(0px)';
+    }
 });
 
 }); //document
